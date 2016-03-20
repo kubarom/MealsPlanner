02e3361a22289e6cda9050594733106f5c7c0f54
Jakub Romaniec <jakub.romaniec@outlook.com>
2016-03-20 01:13:50 +0100

    Added separate Layout file and ViewStart

---
+++ MealsPlanner.Main/App_Start/BundleConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Optimization;
+
+namespace MealsPlanner.Main
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //Further configuraion required as below
+            //           bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                                                    //"~/Scripts/jquery.unobtrusive*",
+                                                    //"~/Scripts/jquery.validate*"));
+        }
+    }
+}
